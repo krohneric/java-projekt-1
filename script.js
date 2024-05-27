46771ea282d13117a8deaea36ffe9318bf17bf66
krohneric <164430768+krohneric@users.noreply.github.com>
2024-05-27 09:30:13 +0000

    working in the script right now

---
+++ script.js
@@ -0,0 +1,23 @@
+const startButton = documnet.getElementById('start-btn')
+
+startButton.addEventListner('click', startGame)
+
+
+
+function startGame() {
+ console.log('Started')
+
+}
+
+
+
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer(){
+
+}
+
+
