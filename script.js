d15f8c68e591baf8e3bda073fd35fd25cd1d6938
krohneric <164430768+krohneric@users.noreply.github.com>
2024-05-27 13:38:44 +0000

    make a new question

--- script.js
+++ script.js
@@ -1,118 +1,103 @@
-const startButton = documnet.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 
-const questionContainerElement = document.getElementById
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
-('question-container')
+let shuffledQuestions, currentQuestionIndex;
 
-const questionElement = documnet.getElementById('question')
-const answerButtonsElement = documnet.getElementById('answer-buttons')
-
-let shuffledQuestions, currentQuestionIndex
-
-startButton.addEventListener('click',startgame)
-nextButton.addEventListener('click',() => {
-currentQuestionIndex++
-setNextQuestion()
-})
-
-
-
-startButton.addEventListner('click', startGame)
-
-
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
- console.log('Started')
- startButton.classList.add('hide') kolla denna sen
- shuffledQuestions = questions.sort(() => Math.random() - .5)
- currentQuestionIndex = 0
- questionContainerElement.classList.remove('hide')
- setNextQuestion()
+  console.log('Started');
+  startButton.classList.add('hide');
+  shuffledQuestions = questions.sort(() => Math.random() - .5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
 }
 
-function resetState(){
-  nextButton.classList.add ('hide') 
-  while (answerButtonsElement.firstChild){
-   answerButtonsElement.removeChild
-   (answerButtonsElement.firstChild)
-  
+function resetState() {
+  nextButton.classList.add('hide');
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
 }
-}
-
-
-
 
 function setNextQuestion() {
- resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
-function selectAnswer(){
-const selectedButton = e.target
-const correct = selectedButton.dataset.correct
-setStatusClass(documnet.body, correct)
-Array.from(answerButtonsElement.children).forEach(button =>{
- setStatusClass (button, button.dataset.correct)
-if (shuffledQuestions.length> currentQuestionIndex + 1)
-
-})
-
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide');
+  }
 }
 
-nextButton.classList.remove('hide')
-
-function setStatusClass(element, correct){
-  clearStatusClass(element)
-¨ if (correct){
-    element.classList.add('correct')
-} else{
-  element.classList.add('wrong')
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add('correct');
+  } else {
+    element.classList.add('wrong');
+  }
 }
 
-
-function clearStatusClass(element){
-element.classList.remove('correct')
-element.classList.remove('wrong')
-
-
+function clearStatusClass(element) {
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
 }
 
-
-
-
-
-
-
-function showQuestion(question){
-questionElement.innerText = question.question
-question.answers.forEach(answer =>
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if(answer.correct){
-       button.dataset.correct  = answer.correct
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
     }
-       button.addEventListener('click', selectAnswer)
-       answerButtonsElement.appendChild(button)
-    ){
-
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
 }
 
-}
-
-
-
-const questions =[
-   {
-    question: 'what s the name of swedish king',
-    answers:[
-        {text: '4', correct:true}
-        {text:'22', correct: false}   
+const questions = [
+  {
+    question: "What is the name of the Swedish king?",
+    answers: [
+      { text: "Carl XVI Gustaf", correct: true },
+      { text: "Gustaf IV Adolf", correct: false }
     ]
+  }
+];
 
-}
 
-]
+const questions = [
+  {
+    question: "what is my favorite band?",
+    answers: [
+      { text: "black stone cherry", correct: true },
+      { text: "hailstrom", correct: false }
+    ]
+  }
+];
+
+
+
+
+
 
 
